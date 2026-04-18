8cabb7d3844af09b812ede7f2c894b08ab417d71
Darshan K G <kgdarshan2005@gmail.com>
2026-04-18 22:47:29 +0530

    Delete tracker directory

--- tracker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import login_view, dashboard
-
-urlpatterns = [
-    path('', login_view, name='login'),
-    path('dashboard/', dashboard, name='dashboard'),
-]
